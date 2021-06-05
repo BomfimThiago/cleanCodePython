771b2e0d44742d69d5d17afc56b29b27ff32805c
Thiago Bomfim <thiago.bomfim@smartgreen.net>
2021-06-05 00:43:30 -0300

    changes

--- calculate_bmi.py
+++ calculate_bmi.py
@@ -2,7 +2,6 @@
 import bisect
 from math import pow
 def bmi(weight, height):
-    #your code here
     breakpoints = [18.5, 25.0, 30.0]
     response = ['Underweight', 'Normal', 'Overweight', 'Obese']
     bmi = weight/pow(height,2)
